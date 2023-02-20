cb565d8899bdc7422b11fca0b1f7b172017287b2
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-02-20 23:57:59 +0000

    started on the jqueery code for displaying the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,69 @@
 let questions= [
  { q: "Which symbol is commonly associated with JQueery?",
     answer: {
-        a: `&`
-        b: `£`
-        c: `$`
-        d: `%`
+        a: `&`,
+        b: `£`,
+        c: `$`,
+        d: `%`,
     }, 
 
     rightAnswer: `c`
-    } 
+    } ,
     { q: "Which tag would you use to input Javascript directly into and HTML page?",
     answer: {
-        a: `<javascript>`
-        b: `<script>`
-        c: `<js>`
-        d: `<JavaScript>`
+        a: `<javascript>`,
+        b: `<script>`,
+        c: `<js>`,
+        d: `<JavaScript>`,
     }, 
 
     rightAnswer: `b`
-    } 
-    { question: "Which of these data types does NOT exist in Javascript?",
+    } ,
+    { q: "Which of these data types does NOT exist in Javascript?",
     answer: {
-        a: `Boolean`
-        b: `Null`
-        c: `Object`
-        d: `Unidentified`
+        a: `Boolean`,
+        b: `Null`,
+        c: `Object`,
+        d: `Unidentified`,
     }, 
 
     rightAnswer: `d`
-    } 
-    { question: "What does push() do in Javascript?",
+    } ,
+    { q: "What does push() do in Javascript?",
     answer: {
-        a: `adds an item into an array`
-        b: `adds the values in an array together`
-        c: `pushes an item to another page`
-        d: `displays your local storage on the live serve html page`
+        a: `adds an item into an array`,
+        b: `adds the values in an array together`,
+        c: `pushes an item to another page`,
+        d: `displays your local storage on the live serve html page`,
     }, 
 
     rightAnswer: `a`
-    } 
-    { question: "Which of the following cannot be used for variable declaration Javascript?",
+    } ,
+    { q: "Which of the following cannot be used for variable declaration Javascript?",
     answer: {
-        a: `for`
-        b: `let`
-        c: `const`
-        d: `var`
+        a: `for`,
+        b: `let`,
+        c: `const`,
+        d: `var`,
     }, 
 
     rightAnswer: `a`
-    } 
+    } ,
 ]
 
-function createQuiz(qs, quizDiv, resultsDiv, submitBtn){
+console.log(questions.length)
 
-	function displayQuestions(qs, quizDiv){
-		
-	}
 
-	function displayResults(qs, quizDiv, resultsDiv){
-		
-	}
 
-	// display the qs
-	displayQuestions(qs, quizDiv);
+ $(function displayQuestions(qs, quizDiv){
+ for(let i=0; i<questions.length; i++ ){
 
-	// when user clicks submit, show results
-	submitBtn.onclick = function(){
-		displayResults(qs, quizDiv, resultsDiv);
-	}
-}
+    questions.answer.forEach(function(){
+    quesitonsDiv= $("<button>")
+    $("button").append(questions.answer[i])
+
+   })
+ }
+    })
+
+displayQuestions(qs, quizDiv);
