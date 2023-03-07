658b6303abdeac64f6202c213cc3b7fc34fe6cfe
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-03-07 21:07:10 +0000

    finishedgit add . Scores now display

--- assets/js/results.js
+++ assets/js/results.js
@@ -5,3 +5,19 @@ function clearr() {
     window.localStorage.clear();
    
   }
+
+let items = { ...localStorage }
+
+window.localStorage.removeItem("debug")
+window.localStorage.removeItem("correct answer")
+
+ let keys =Object.keys(window.localStorage)
+for (i=0; i<keys.length && i <4; i++){
+var litem= document.createElement("li")
+litem.textContent = keys[i] + "   " + window.localStorage.getItem(keys[i])
+let highscores=document.getElementById("highscores")
+highscores.append(litem)
+
+
+}
+
