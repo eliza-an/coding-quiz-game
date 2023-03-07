1b2b6cc6144c7c2e34f63525255a04cb38b39774
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-03-07 21:23:32 +0000

    final tweaks

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,5 +1,5 @@
 //let clearr= document.getElementById("clearr")
-
+removing()
 //clearr.addEventListener("click", window.localStorage.clear)
 function clearr() {
     window.localStorage.clear();
@@ -8,8 +8,9 @@ function clearr() {
 
 let items = { ...localStorage }
 
-window.localStorage.removeItem("debug")
-window.localStorage.removeItem("correct answer")
+function removing(){
+  window.localStorage.removeItem("debug")
+window.localStorage.removeItem("correct answer")}
 
  let keys =Object.keys(window.localStorage)
 for (i=0; i<keys.length && i <4; i++){
